71e6128630f14137aab8232c27a1cdc30d0e3c64
shuckiy73 <121363089+shuckiy73@users.noreply.github.com>
2024-10-17 09:04:40 +0300

    modified

--- cooking/forms.py
+++ cooking/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class PostAddForm(forms.ModelForm):
     """форма для добавления статьи от пользователя"""
@@ -14,3 +15,13 @@ class PostAddForm(forms.ModelForm):
             'photo': forms.FileInput(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'})
         }
+
+class LoginForm(AuthenticationForm):
+    """ Форма для аутентификации пользователя"""
+    username = forms.CharField(label='Имя пользователя', 
+                               max_length=150, 
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    password = forms.CharField(label='Пароль', 
+                               max_length=150, 
+                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
