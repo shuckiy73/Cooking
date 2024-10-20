802d5e2a91096f92c39a3d9796a139b4598526df
shuckiy73 <121363089+shuckiy73@users.noreply.github.com>
2024-10-20 20:55:35 +0300

    modified

--- cooking/urls.py
+++ cooking/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import (
     Index, ArticleByCategory, PostDetail, AddPost, PostUpdate, PostDelete,
-    SearchResult, user_login, user_logout, register, add_comment, profile
+    SearchResult, user_login, user_logout, register, add_comment, profile,
+    UserChangePassword, CookingAPI, CookingAPIDetail, CookingCategoryAPI, CookingCategoryAPIDetail  # Добавьте импорт CookingCategoryAPI и CookingCategoryAPIDetail
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
 )
 
 urlpatterns = [
@@ -40,4 +46,20 @@ urlpatterns = [
 
     # Профиль пользователя
     path('profile/<int:user_id>/', profile, name='profile'),
+
+    # Смена пароля
+    path('password/', UserChangePassword.as_view(), name='change_password'),
+
+    # API для статей
+    path('posts/api/', CookingAPI.as_view(), name='CookingAPI'),
+    path('posts/api/<int:pk>/', CookingAPIDetail.as_view(), name='CookingAPIDetail'),  # Исправленный путь
+
+    # API для категорий
+    path('categories/api/', CookingCategoryAPI.as_view(), name='CookingCategoryAPI'),
+    path('categories/api/<int:pk>/', CookingCategoryAPIDetail.as_view(), name='CookingCategoryAPIDetail'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
